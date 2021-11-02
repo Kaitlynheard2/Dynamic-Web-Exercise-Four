8b959914afe14ae49de5e50db54e71e4f6b8c497
MyMac <mymac@10-18-23-55.dynapool.wireless.nyu.edu>
2021-11-02 08:59:52 -0400

    started setting up firebase

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 4000;
+const firebase = require("firrebase/app");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxm6_253NWqCgZ87rQjz_z2u2PlvZPN5w",
+  authDomain: "kaitlyn-exercise-four.firebaseapp.com",
+  projectId: "kaitlyn-exercise-four",
+  storageBucket: "kaitlyn-exercise-four.appspot.com",
+  messagingSenderId: "827213467323",
+  appId: "1:827213467323:web:d9c40e3a23e366dfda239e",
+  measurementId: "G-3XYCYQHVYV",
+};
+
+const app = inititializeApp(firebaseConfig);
 
 const indexRoute = require("./routers/index");
 const articleRoute = require("./routers/article");
--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+const firebase = require("firebase/firestore/lite");
+const db = firebase.firestore();
+const blogposts = db.collection("blogposts");
+
 router.get("/", (req, res) => {
-  res.send(
-    `<h1>All Articles</h1>
-  <p>Articles coming soon... need to set up firebase</p>`
-  );
+  //create empty array
+  const blogpostsArray = [];
+  //Get blogposts JSON from Firebase
+  //Push document form blogposts into blogposts array...
+  res.send([]);
 });
 
 module.exports = router;
